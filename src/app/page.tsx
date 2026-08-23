9ca74869cf651c92dd4f81c5b5fea8260d9c98c0
ndorsett333 <nicolasdorsett@gmail.com>
2026-08-23 14:16:10 -0600

    set up eslint & prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
+            To get started, edit the <code className={styles.code}>page.tsx</code> file.
           </h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
