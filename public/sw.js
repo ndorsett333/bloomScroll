9d9bf43a49a7056fe1f653b3a2d2406362600bc9
ndorsett333 <nicolasdorsett@gmail.com>
2026-08-24 19:38:20 -0600

    configure PWA support

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "bloomscroll-v1";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.svg", "/icon-512.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      ),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then((response) => response || caches.match("/"))),
+  );
+});
